f23c2e85e292d7b034269a028062d190960917aa
michaeloyer <michaeloyer@users.noreply.github.com>
2017-02-04 12:18:31 -0500

    Add extenion methods for generic collections

--- ExtraLinq/Extensions.cs
+++ ExtraLinq/Extensions.cs
@@ -1,8 +1,19 @@
+﻿using System.Collections.Generic;
 
 namespace ExtraLinq
 {
     public static class Extensions
     {
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> values) =>
+            new HashSet<T>(values);
 
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> values, IEqualityComparer<T> comparer) =>
+            new HashSet<T>(values, comparer);
+
+        public static Queue<T> ToQueue<T>(this IEnumerable<T> values) =>
+            new Queue<T>(values);
+
+        public static Stack<T> ToStack<T>(this IEnumerable<T> values) =>
+            new Stack<T>(values);
     }
 }
